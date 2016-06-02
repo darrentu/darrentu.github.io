babce99ecc14ba698147d43c715ed38f90e91bcb
darrentu <ddaarreenn@hotmail.com>
2016-06-02 19:11:50 -0400

    initial commit

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+  	$('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+ $('.anim').hover(
+       function(){ $(this).addClass('z-depth-5') },
+       function(){ $(this).removeClass('z-depth-5') }
+)
